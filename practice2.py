0e5273f76e3ef42d054e9aa4ac0b8a7885eaff46
jahidstm <jahidhasanstm@gmail.com>
2025-08-06 21:08:27 +0600

    Matplotlib

---
+++ practice2.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+x = ['Sun', 'Mon', 'Thurs', 'Wed']
+y = [20, 30, 25, 40]
+
+plt.plot(x,y)
+plt.title("Day to day calculation")
+plt.xlabel("X axis")
+plt.ylabel("Y axis")
+plt.show()
