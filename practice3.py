3887ef0b9a33922b9a4866eee17eeba5d1b68b0b
jahidstm <jahidhasanstm@gmail.com>
2025-08-08 23:34:59 +0600

    Matplotlib

--- practice3.py
+++ practice3.py
@@ -12,5 +12,6 @@ plt.grid(color='gray', linestyle=':', linewidth=1)
 plt.xlim(1,4)
 plt.ylim(1,3000)
 plt.xticks([1,2,3,4],['M1','M2','M3','M4',])
+plt.yticks([1000, 1500, 2500, 2000],['S1','S2','S3','S4',])
 
 plt.show()
